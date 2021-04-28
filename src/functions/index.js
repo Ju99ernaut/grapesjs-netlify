398909628bcfb269afafb274e89319d675a54ba9
Ju99ernaut <brendonzee@gmail.com>
2021-04-28 15:17:08 +0200

    Add .env manager

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,4 +1,5 @@
 import flowchart from './flowchart';
+import compiler from './compiler';
 
 export default (editor, opts = {}) => {
     const mdl = editor.Modal;
@@ -6,9 +7,29 @@ export default (editor, opts = {}) => {
     const pfx = editor.getConfig('stylePrefix');
     const mdlClass = `${pfx}mdl-dialog-ntl`;
     const { $el, flowEditor, setZoom } = flowchart(editor, opts);
+    const { keys } = Object;
 
     setZoom();
     editor.NetlifyDashboard.flowEditor = flowEditor;
+    editor.NetlifyDashboard.env = {};
+    editor.NetlifyDashboard.setEnv = (env) => {
+        editor.NetlifyDashboard.env = { ...editor.NetlifyDashboard.env, ...env };
+    };
+    editor.NetlifyDashboard.getEnv = () => editor.NetlifyDashboard.env;
+    editor.NetlifyDashboard.getDotEnv = () => {
+        let file = ''
+        keys(editor.NetlifyDashboard.env).forEach(env => {
+            file += `${env}=${editor.NetlifyDashboard.env[env]} \n`;
+        });
+        return file;
+    }
+    editor.getFunctions = () => {
+        return {
+            '.env': editor.NetlifyDashboard.getDotEnv,
+            'package.json': '{"name":"backend-functions","version":"0.1.0","private":true,"dependencies":{"node-fetch":"^2.3.0"}}',
+            ...compiler(flowEditor.export())
+        }
+    };
 
     cm.add('netlify-functions', {
         run(editor, sender) {
